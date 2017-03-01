4ec4c21153dbc5c3ffdd7a732ff39be0aa42868d
mgill2 <mgill5@hawk.iit.edu>
2017-03-01 11:44:55 -0600

    added the if statements to see if the commands executed correctly

--- scripts/database-server.sh
+++ scripts/database-server.sh
@@ -8,13 +8,37 @@ sed 's/pleasechangeme/database/' /etc/hostname
 
 #updating the system
 sudo apt-get update -y
+if [ $? = 0 ]
+then
+  echo "update completed"
+else
+  echo "update did NOT complete"
+end
 
 #adding the sql server
 sudo apt-get install -y mysql-server git curl
+if [ $? = 0 ]
+then
+  echo "update completed"
+else
+  echo "update did NOT complete"
+end
 
 #changing the address to webserver address
 sed '40,55s/0.0.0.0/192.168.1.217' /etc/mysql/my.cnf
+if [ $? = 0 ]
+then
+  echo "update completed"
+else
+  echo "update did NOT complete"
+end
 
 #reloading mysql so that it takes the new ip
 sudo service mysql reload
+if [ $? = 0 ]
+then
+  echo "update completed"
+else
+  echo "update did NOT complete"
+end
 
