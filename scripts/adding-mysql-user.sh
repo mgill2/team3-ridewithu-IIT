7c31b8ae193e10cde59e54b7fb69038fa5f4a5b3
mgill2 <mgill5@hawk.iit.edu>
2017-03-06 11:35:50 -0600

    moved echo

--- scripts/adding-mysql-user.sh
+++ scripts/adding-mysql-user.sh
@@ -6,11 +6,12 @@ usersize=${username[*]}
 
 PASSWD="password"
 
+ echo "Please enter root password"
+ read rootpasswd
 
 for((i=0;i<usersize;i++))
 
-  echo "Please enter root password"
-  read rootpasswd
+ 
   
   mysql -u root -p$rootpasswd -e "CREATE USER ${username}@localhost IDENTIFIED BY '${PASSWD}';"
   #printf "${username}[i]"
