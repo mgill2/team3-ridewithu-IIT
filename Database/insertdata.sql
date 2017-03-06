834587474b5fad0a05decfd0747fb86ca054bede
ddelvadi <ddelvadi@hawk.iit.edu>
2017-03-06 00:17:54 -0600

    Inserted data of 5 rows

---
+++ Database/insertdata.sql
@@ -0,0 +1,8 @@
+USE backend;
+
+INSERT INTO backend (ID, FIRST_NAME, LAST_NAME, EMAIL) values 
+(1, "Dipen", "Delvadiya", "ddelvadi@hawk.iit.edu"),
+(2, "Matthew", "Gill", "mgill5@hawk.iit.edu"),
+(3, "Alejandro", "Gomez", "agomez15@hawk.iit.edu"),
+(4, "Zdenek", "Jaks", "zjaks@hawk.iit.edu"),
+(5, "Paul", "Perumalla", "pperumal@hawk.iit.edu");
