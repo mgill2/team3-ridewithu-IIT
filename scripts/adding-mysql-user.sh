47f2018d393682d0fc08caaa76ef53244df7bab5
mgill2 <mgill5@hawk.iit.edu>
2017-03-06 11:16:42 -0600

    added if else statement

--- scripts/adding-mysql-user.sh
+++ scripts/adding-mysql-user.sh
@@ -9,7 +9,15 @@ PASSWD="password"
 
 for((i=0;i<usersize;i++))
 
+if [ -f /root/.my.cnf ]; then
+  
   mysql -e "CREATE USER ${username}@localhost IDENTIFIED BY '${PASSWD}';"
-  printf "${username}[i]"
+  #printf "${username}[i]"
+else
+  
+  echo "Please enter root user MySQL password!"
+  read rootpasswd
+  mysql -uroot -p$(rootpasswd) -e "CREATE USER ${username}@localhost IDENTIFIED BY '${PASSWD}';"
 
+fi
 done  
