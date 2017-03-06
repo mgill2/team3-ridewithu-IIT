573049b091d08473d14172e2894d60986f1de5f8
mgill2 <mgill5@hawk.iit.edu>
2017-03-06 09:40:20 -0600

    removed the id values becuase of auto increment

--- Database/insertdata.sql
+++ Database/insertdata.sql
@@ -1,8 +1,8 @@
 USE backend;
 
 INSERT INTO items (ID, FIRST_NAME, LAST_NAME, EMAIL) values 
-(1, "Dipen", "Delvadiya", "ddelvadi@hawk.iit.edu"),
-(2, "Matthew", "Gill", "mgill5@hawk.iit.edu"),
-(3, "Alejandro", "Gomez", "agomez15@hawk.iit.edu"),
-(4, "Zdenek", "Jaks", "zjaks@hawk.iit.edu"),
-(5, "Paul", "Perumalla", "pperumal@hawk.iit.edu");
+("Dipen", "Delvadiya", "ddelvadi@hawk.iit.edu"),
+("Matthew", "Gill", "mgill5@hawk.iit.edu"),
+("Alejandro", "Gomez", "agomez15@hawk.iit.edu"),
+("Zdenek", "Jaks", "zjaks@hawk.iit.edu"),
+("Paul", "Perumalla", "pperumal@hawk.iit.edu");
