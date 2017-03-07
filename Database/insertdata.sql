8007e13e59e64e6fb32b70bca3bf7a6ff6dd63b0
mgill2 <mgill5@hawk.iit.edu>
2017-03-06 21:51:48 -0600

    got rid of the 0's for autoincrement

--- Database/insertdata.sql
+++ Database/insertdata.sql
@@ -1,8 +1,8 @@
 USE backend;
 
 INSERT INTO items (ID, FIRST_NAME, LAST_NAME, EMAIL) values 
-(0,"Dipen", "Delvadiya", "ddelvadi@hawk.iit.edu"),
-(0,"Matthew", "Gill", "mgill5@hawk.iit.edu"),
-(0,"Alejandro", "Gomez", "agomez15@hawk.iit.edu"),
-(0,"Zdenek", "Jaks", "zjaks@hawk.iit.edu"),
-(0,"Paul", "Perumalla", "pperumal@hawk.iit.edu");
+("Dipen", "Delvadiya", "ddelvadi@hawk.iit.edu"),
+("Matthew", "Gill", "mgill5@hawk.iit.edu"),
+("Alejandro", "Gomez", "agomez15@hawk.iit.edu"),
+("Zdenek", "Jaks", "zjaks@hawk.iit.edu"),
+("Paul", "Perumalla", "pperumal@hawk.iit.edu");
