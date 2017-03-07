0a818e9c2b4abcf2ab5d82cef3a0f87bbb2e911a
mgill2 <mgill5@hawk.iit.edu>
2017-03-07 15:31:31 -0600

    added vagrant commands to powershell script

---
+++ scripts/vagrant-setup.ps1
@@ -0,0 +1,23 @@
+#going to the directory for database
+
+cd "../build"
+
+#running add box command
+vagrant.exe box add --name database-server ubuntu-vanilla-14045-server-database-virtualbox.box
+
+#getting box up and running
+cd "./database"
+vagrant.exe init database-server
+vagrant.exe up 
+vagrant.exe ssh
+
+#changing directories
+cd "../"
+
+#adding webserver box to box list
+vagrant.exe box add --name webserver ubuntu-vanilla-14045-webserver-virtualbox.box
+
+#getting box up and running
+cd "./webserver"
+
+vagrant.exe init ubuntu-vanilla-14045-webserver-virtualbox.box
