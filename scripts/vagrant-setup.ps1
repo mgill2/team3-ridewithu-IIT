9eb5ef58d910e55c0da9da4321fba9c5a20af45c
mgill2 <mgill5@hawk.iit.edu>
2017-03-07 20:14:26 -0600

    added --name after the build

--- scripts/vagrant-setup.ps1
+++ scripts/vagrant-setup.ps1
@@ -3,7 +3,7 @@
 cd "../build"
 
 #running add box command
-vagrant.exe box add --name database-server ubuntu-vanilla-14045-server-database-virtualbox.box
+vagrant.exe box add ./ubuntu-vanilla-14045-database-virtualbox.box --name database-server
 
 #getting box up and running
 cd "./database"
@@ -11,16 +11,16 @@ vagrant.exe init database-server
 vagrant.exe up 
 vagrant.exe ssh
 
-#changing directories
+#changing directories to build
 cd "../"
 
 #adding webserver box to box list
-vagrant.exe box add --name webserver ubuntu-vanilla-14045-webserver-virtualbox.box
+vagrant.exe box add ubuntu-vanilla-14045-webserver-virtualbox.box --name webserver2
 
 #getting box up and running
 cd "./webserver"
 
-vagrant.exe init webserver
+vagrant.exe init webserver-2
 vagrant.exe up
 vagrant.exe ssh
 
