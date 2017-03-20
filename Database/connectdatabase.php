0a734e268573e3dd59d28a39e25d0616f2ad4ea5
ddelvadi <ddelvadi@hawk.iit.edu>
2017-03-20 16:01:43 -0500

    made external config-database.php file to save credentials

--- Database/connectdatabase.php
+++ Database/connectdatabase.php
@@ -1,13 +1,4 @@
 <?php
-//conection: 
-$link = mysqli_connect("192.168.1.208","team3rocks","jeremytheboss","backend") or die("Error " . mysqli_error($link)); 
-echo "Here is the result: " . $link;
- /*$sql = "CREATE TABLE comments 
-(
-ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-PosterName VARCHAR(32),
-Title VARCHAR(32),
-Content VARCHAR(500)
-)";
-$con->query($sql); */
+include_once 'config_database.php'; //Calling all the variables saved in this file
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 ?>
