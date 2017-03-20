edc5df6d0b61140054437533ef97290776ae30f6
ddelvadi <ddelvadi@hawk.iit.edu>
2017-03-20 16:04:13 -0500

    Added password and username fields in plain text for now

--- Database/insertdata.sql
+++ Database/insertdata.sql
@@ -1,8 +1,8 @@
 USE backend;
 
 INSERT INTO items (ID, FIRST_NAME, LAST_NAME, EMAIL) values 
-("Dipen", "Delvadiya", "ddelvadi@hawk.iit.edu"),
-("Matthew", "Gill", "mgill5@hawk.iit.edu"),
-("Alejandro", "Gomez", "agomez15@hawk.iit.edu"),
-("Zdenek", "Jaks", "zjaks@hawk.iit.edu"),
-("Paul", "Perumalla", "pperumal@hawk.iit.edu");
+("Dipen", "Delvadiya", "ddelvadi@hawk.iit.edu", "ddelvadi", "DipenDelvadiya"),
+("Matthew", "Gill", "mgill5@hawk.iit.edu", "mgill", "MathewGill"),
+("Alejandro", "Gomez", "agomez15@hawk.iit.edu", "agomez", "AGomez"),
+("Zdenek", "Jaks", "zjaks@hawk.iit.edu", "zjaks", "ZJaks"),
+("Paul", "Perumalla", "pperumal@hawk.iit.edu", "pperumal", "Paul");
