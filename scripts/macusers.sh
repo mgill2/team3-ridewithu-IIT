1a45c3ce66981589337fc0a0d0bc7fe37f6a3f2b
mgill2 <mgill5@hawk.iit.edu>
2017-03-20 11:28:36 -0500

    added LF for unix

--- scripts/macusers.sh
+++ scripts/macusers.sh
@@ -1,1 +1,33 @@
-#!/bin/bash -xe#make sure to chmod this script to run#adding boxes for vagrant#changing directorycd "../build"#adding database boxesvagrant box add --name database-server ./ubuntu-vanilla-14045-database-virtualbox.box#getting box up and runningcd "./database"vagrant init database-servervagrant up vagrant ssh#changing directories to buildcd "../"#adding webserver box to box listvagrant box add ubuntu-vanilla-14045-webserver-virtualbox.box --name webserver2#getting box up and runningcd "./webserver"vagrant init webserver2vagrant upvagrant ssh
+#!/bin/bash -xe
+
+#make sure to chmod this script to run
+#adding boxes for vagrant
+
+#changing directory
+cd "../build"
+
+#adding database boxes
+vagrant box add --name database-server ./ubuntu-vanilla-14045-database-virtualbox.box
+
+#getting box up and running
+cd "./database"
+vagrant init database-server
+vagrant up 
+vagrant ssh
+
+#changing directories to build
+cd "../"
+
+#adding webserver box to box list
+vagrant box add ubuntu-vanilla-14045-webserver-virtualbox.box --name webserver2
+
+#getting box up and running
+cd "./webserver"
+
+vagrant init webserver2
+vagrant up
+vagrant ssh
+
+
+
+
