4fe8070888274112823bd04e29d21e7b5e8ec786
ddelvadi <ddelvadi@hawk.iit.edu>
2017-03-20 22:25:57 -0500

    defined session_set_cookie_params function

--- Database/functions.php
+++ Database/functions.php
@@ -5,10 +5,19 @@
     $session_name = 'sec_session_id';
     $secure = true; 
     $httponly = true;
-  }
 
   if (ini_set('session.use_only_cookies', 1) === FALSE) {
       header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
       exit();
     }
+
+   session_set_cookie_params($cookieParams["lifetime"],
+        $cookieParams["path"], 
+        $cookieParams["domain"], 
+        $secure,
+        $httponly);
+ 
+    session_start();            // Start the PHP session 
+    session_regenerate_id(true);    // regenerated the session, delete the old one. 
+}
 ?>
