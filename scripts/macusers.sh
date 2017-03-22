917804d5b963727bf15692743fc4f7a4ae94462a
mgill2 <mgill5@hawk.iit.edu>
2017-03-22 09:40:31 -0500

    added echo and read to specify the name you want for the vagrant boxes

--- scripts/macusers.sh
+++ scripts/macusers.sh
@@ -7,23 +7,27 @@
 cd "../build"
 
 #adding database boxes
-vagrant box add --name database-server ./ubuntu-vanilla-14045-database-virtualbox.box
+echo "Please enter the name of your box you would like to create for the database. Example: database-server"
+read name
+vagrant box add --name $name ./ubuntu-vanilla-14045-database-virtualbox.box
 
 #getting box up and running
 cd "./database"
-vagrant init database-server
+vagrant init $name
 vagrant up
 
 #changing directories to build
 cd "../"
 
 #adding webserver box to box list
-vagrant box add ubuntu-vanilla-14045-webserver-virtualbox.box --name webserver2
+echo "Please enter the name of your box you would like to create for the webserver. Example: webserver"
+read name2
+vagrant box add --name $name2 ubuntu-vanilla-14045-webserver-virtualbox.box 
 
 #getting box up and running
 cd "./webserver"
 
-vagrant init webserver2
+vagrant init $name2
 vagrant up
 
 
