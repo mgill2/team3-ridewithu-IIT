633d0868b5dd9e5a530c2587bd7dfc21fd12a2a5
ddelvadi <ddelvadi@hawk.iit.edu>
2017-03-24 02:52:24 -0500

    Added 0 so it script wont run into any error when compiled

--- Database/insertdata.sql
+++ Database/insertdata.sql
@@ -1,8 +1,8 @@
 USE backend;
 
 INSERT INTO items (ID, FIRST_NAME, LAST_NAME, EMAIL, USERNAME, PASSWORD) values 
-("Dipen", "Delvadiya", "ddelvadi@hawk.iit.edu", "ddelvadi", "DipenDelvadiya"),
-("Matthew", "Gill", "mgill5@hawk.iit.edu", "mgill", "MathewGill"),
-("Alejandro", "Gomez", "agomez15@hawk.iit.edu", "agomez", "AGomez"),
-("Zdenek", "Jaks", "zjaks@hawk.iit.edu", "zjaks", "ZJaks"),
-("Paul", "Perumalla", "pperumal@hawk.iit.edu", "pperumal", "Paul");
+(0,"Dipen", "Delvadiya", "ddelvadi@hawk.iit.edu", "ddelvadi", "DipenDelvadiya"),
+(0,"Matthew", "Gill", "mgill5@hawk.iit.edu", "mgill", "MathewGill"),
+(0,"Alejandro", "Gomez", "agomez15@hawk.iit.edu", "agomez", "AGomez"),
+(0,"Zdenek", "Jaks", "zjaks@hawk.iit.edu", "zjaks", "ZJaks"),
+(0,"Paul", "Perumalla", "pperumal@hawk.iit.edu", "pperumal", "Paul");
