60779e6913959d0c1a9d660b74dcaac6070f0673
ddelvadi <ddelvadi@hawk.iit.edu>
2017-03-24 02:31:04 -0500

    Added missing fields (Email and Password)

--- Database/insertdata.sql
+++ Database/insertdata.sql
@@ -1,6 +1,6 @@
 USE backend;
 
-INSERT INTO items (ID, FIRST_NAME, LAST_NAME, EMAIL) values 
+INSERT INTO items (ID, FIRST_NAME, LAST_NAME, EMAIL, USERNAME, PASSWORD) values 
 ("Dipen", "Delvadiya", "ddelvadi@hawk.iit.edu", "ddelvadi", "DipenDelvadiya"),
 ("Matthew", "Gill", "mgill5@hawk.iit.edu", "mgill", "MathewGill"),
 ("Alejandro", "Gomez", "agomez15@hawk.iit.edu", "agomez", "AGomez"),
