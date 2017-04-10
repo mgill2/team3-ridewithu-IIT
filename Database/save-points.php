68e1e9faa7a35f25b0a83c54d19ba706d53041ca
ddelvadi <ddelvadi@hawk.iit.edu>
2017-04-10 01:24:46 -0500

    Successfully added session variables and corrected all typos

--- Database/save-points.php
+++ Database/save-points.php
@@ -1,6 +1,12 @@
 <?php
+session_start();
+	$origin_address= " ";
+	$destination_address= " ";
 	$origin_address = $_POST['origin_address'];
 	$destination_address = $_POST['destination_address'];
 	echo $origin_address;
 	echo $destination_address;
+	$_SESSION['destination'] = $destination_address;
+	$_SESSION['origin'] = $origin_address;
+
 ?>
