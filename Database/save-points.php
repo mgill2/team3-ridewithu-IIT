bca832081f2d14eee12f94b0fcf2c06aff0e4d2e
ddelvadi <ddelvadi@hawk.iit.edu>
2017-04-10 01:08:36 -0500

    Added PHP file that handles AJAX call and set session variables

--- Database/save-points.php
+++ Database/save-points.php
@@ -1,6 +1,6 @@
 <?php
-	$origin_address = $_GET['origin_address'];
-	$destination_address = $GET['destination_address'];
+	$origin_address = $_POST['origin_address'];
+	$destination_address = $_POST['destination_address'];
 	echo $origin_address;
 	echo $destination_address;
 ?>
