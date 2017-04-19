d87c400b67627ef21f26f2fb5cfe697c0bdae5f6
ddelvadi <ddelvadi@hawk.iit.edu>
2017-04-18 23:21:30 -0500

    Added 4 more field in AJAX call so store those variables in SESSION

--- src/js/request.js
+++ src/js/request.js
@@ -95,14 +95,25 @@ $(document).ready( function() {
 
     origin_addr = $("#origin-input").val(); 
     destination_addr = $("#destination-input").val();
-    console.log(destination_addr);
+    datevalue = $("#datepicker").val();
+    hour = $("#hour").val();
+    minutes = $("#minutes").val();
+    clock = $("#clock").val();
+    //console.log(destination_addr);
+    //console.log(hour);
+    console.log(clock);
+    console.log(datevalue);
 
             $.ajax({
               type: "POST",
               url: "../scripts/Database/save-points.php",
               data: { 
                 origin_address: origin_addr,
-                destination_address: destination_addr
+                destination_address: destination_addr,
+                leaving_date: datevalue,
+                leaving_hour: hour,
+                leaving_minutes: minutes,
+                leaving_clock: clock
               },
         success: function(html){
           alert(html);
