508e32494a9ceb55b6cf3a995eae8f7750c8d701
mgill2 <mgill5@hawk.iit.edu>
2017-04-19 22:02:32 -0500

    added master log file location, and log position

--- scripts/Database/change-master-to.sql
+++ scripts/Database/change-master-to.sql
@@ -2,8 +2,8 @@ CHANGE MASTER TO
 	master_host='192.168.1.220', 
 	master_user='replicate',
 	master_password='slaveReplicate',
-	master_log_file='', 
-  master_log_pos=4;
+	master_log_file='mysql-bin.000001', 
+  master_log_pos=107;
 
 use master;
 start slave;
