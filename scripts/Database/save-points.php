23d6046f10d988f1eac178ef86e9ff8b8be30625
ddelvadi <ddelvadi@hawk.iit.edu>
2017-04-22 23:43:55 -0500

    Added code to debug application to make sure that it takes all the values from form and pass to PHP successfully

--- scripts/Database/save-points.php
+++ scripts/Database/save-points.php
@@ -4,9 +4,29 @@ session_start();
 	$destination_address= " ";
 	$origin_address = $_POST['origin_address'];
 	$destination_address = $_POST['destination_address'];
+	$leaving_date = $_POST['leaving_date'];
+	$leaving_hour = $_POST['leaving_hour'];
+	$leaving_minutes = $_POST['leaving_minutes'];
+	$leaving_clock = $_POST['leaving_clock'];
 	echo $origin_address;
 	echo $destination_address;
+	echo $leaving_date;
+	echo $leaving_hour;
+	echo $leaving_minutes;
+	echo $leaving_clock;
+
+	$origin_address = mysql_real_escape_string($origin_address);
+	$destination_address = mysql_real_escape_string($destination_address);
+	$leaving_date = mysql_real_escape_string($leaving_date);
+	$leaving_time = $leaving_hour . ':' . $leaving_minutes . ':' . $leaving_clock;
+	$leaving_time = mysql_real_escape_string($leaving_time);
+	echo $leaving_time;
 	$_SESSION['destination'] = $destination_address;
 	$_SESSION['origin'] = $origin_address;
+	$_SESSION['leaving_date'] = $leaving_date;
+	$_SESSION['leaving_hour'] = $leaving_hour;
+	$_SESSION['leaving_minutes'] = $leaving_minutes;
+	$_SESSION['leaving_clock'] = $leaving_clock;
+
 
 ?>
