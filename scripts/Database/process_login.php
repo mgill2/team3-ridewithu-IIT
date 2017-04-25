724a098d01a00e707a746d4bd37eb82eaddefb04
ddelvadi <ddelvadi@hawk.iit.edu>
2017-04-24 22:39:18 -0500

    Took out extra lines and corrected name of the table

--- scripts/Database/process_login.php
+++ scripts/Database/process_login.php
@@ -1,39 +1,21 @@
 <?php
   session_start();
   require_once('connectdatabase.php');
-  /*include_once 'functions.php';
 
-  start_session();
-  
-  if(isset($_POST['email'], $_POST['p'])) {
-  	$email = $_POST['email'];
-  	$password = $_POST['p'];
-
-  	if(login($email, $password, $mysqli) == true) {
-  		//login success
-  		header('Location: ../welcome.php');
-  	}
-  	else {
-  		header('Location: ../index.php?error=1');
-  	}
-  }
-  else {
-  	echo 'Invalid credentials';
-  }
-*/
 if(isset($_POST) && !empty($_POST)) {
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  $sql = "SELECT * FROM items WHERE USERNAME='$username' AND PASSWORD='$password'";
+  $sql = "SELECT * FROM users WHERE USERNAME='$username' AND PASSWORD='$password'";
   $result = mysqli_query($connection, $sql);
   echo $count = mysqli_num_rows($result);
 
   if($count == 1) {
     $_SESSION['username']=$username;
-    header('Location: ../welcome.php');
+    header('Location: ../welcome.html');
   }
   else {
+    echo '<script>alert("Username or Password is incorrect");</script>';  
     header('Location: ../index.php');
   }
 }
