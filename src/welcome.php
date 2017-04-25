611251fccf844b8db49056a717d9b2b0e5536f96
ddelvadi <ddelvadi@hawk.iit.edu>
2017-04-25 00:26:29 -0500

    Took out extra code and added some debug code to check if all the session variables are getting passed or not

--- src/welcome.php
+++ src/welcome.php
@@ -1,27 +1,7 @@
 <?php
-
-// Load the settings from the central config file
-require_once 'includes/CAS/config.php';
-
-// Load the CAS lib
-require_once 'includes/CAS.php';
-
-// Initialize phpCAS
-phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
-
-// For production use set the CA certificate that is the issuer of the cert
-// on the CAS server and uncomment the line below
-phpCAS::setCasServerCACert($cas_server_ca_cert_path);
-
-// force CAS authentication
-phpCAS::forceAuthentication();
-
-// at this step, the user has been authenticated by the CAS server
-// and the user's login name can be read with phpCAS::getUser().
-
-// for this test, simply print that the authentication was successfull
+  $fist_name = $_SESSION['first_name'];
+  echo "<script>console.log('$first_name');</script>";
 ?>
-
 <!DOCTYPE html>
 <html lang="en" class="nojs">
 <head>
