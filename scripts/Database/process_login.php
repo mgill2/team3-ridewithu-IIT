9d63fd76a778e5bf9360e11a0e46e7bbdea7791f
ddelvadi <ddelvadi@hawk.iit.edu>
2017-04-25 00:22:23 -0500

    Defined SESSION variables that take user's first name, last name and email after they are authenticated

--- scripts/Database/process_login.php
+++ scripts/Database/process_login.php
@@ -11,8 +11,12 @@ if(isset($_POST) && !empty($_POST)) {
   echo $count = mysqli_num_rows($result);
 
   if($count == 1) {
+    $row = mysqli_fetch_assoc($result);
+    $_SESSION['first_name'] = $row["FIRST_NAME"];
+    $_SESSION['last_name'] = $row["LAST_NAME"];
+    $_SESSION['email'] = $row["EMAIL"];
     $_SESSION['username']=$username;
-    header('Location: ../../src/welcome.html');
+    header('Location: ../../src/welcome.php');
   }
   else {
     header('Location: ../../src/index.php');
