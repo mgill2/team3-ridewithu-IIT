8c82416874950ef58fcb79f5639065e65488ed55
ddelvadi <ddelvadi@hawk.iit.edu>
2017-04-29 15:28:34 -0500

    Commented out extra code and modified query so it will take name and email of users from session variables

--- scripts/Database/save-points.php
+++ scripts/Database/save-points.php
@@ -9,22 +9,26 @@ session_start();
 	$leaving_hour = $_POST['leaving_hour'];
 	$leaving_minutes = $_POST['leaving_minutes'];
 	$leaving_clock = $_POST['leaving_clock'];
-	echo $origin_address;
-	echo $destination_address;
-	echo $leaving_date;
-	echo $leaving_hour;
-	echo $leaving_minutes;
-	echo $leaving_clock;
+	$name = $_SESSION['name'];
+	$email = $_SESSION['email'];
+	//echo $origin_address;
+	//echo $destination_address;
+	//echo $leaving_date;
+	//echo $leaving_hour;
+	//echo $leaving_minutes;
+	/echo $leaving_clock;
 
 	$origin_address = mysqli_real_escape_string($connection, $origin_address);
 	$destination_address = mysqli_real_escape_string($connection, $destination_address);
 	$leaving_date = mysqli_real_escape_string($connection, $leaving_date);
 	$leaving_time = $leaving_hour . ':' . $leaving_minutes . ' ' . $leaving_clock;
 	$leaving_time = mysqli_real_escape_string($connection, $leaving_time);
-	$query = "INSERT INTO listride (ORIGIN, DESTINATION, LEAVING_DATE, LEAVING_TIME) VALUES ('$origin_address','$destination_address','$leaving_date','$leaving_time')";
+	$name_user = mysqli_real_escape_string($connection, $name);
+	$email_user = mysqli_real_escape_string($connection, $email);
+	$query = "INSERT INTO listride (ID, NAME, EMAIL, ORIGIN, DESTINATION, LEAVING_DATE, LEAVING_TIME) VALUES (0, '$name_user', '$email_user', '$origin_address','$destination_address','$leaving_date','$leaving_time')";
 	$result = mysqli_query($connection, $query);
 	if ($result == true) {
-		echo '<script>console.log("Data inserted")</script>';
+		//echo '<script>console.log("Data inserted")</script>';
 	}
 	$_SESSION['destination'] = $destination_address;
 	$_SESSION['origin'] = $origin_address;
