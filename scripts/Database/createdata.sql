e6d73f22d49319f53b5c4e29e3fb494fa2180d89
ddelvadi <ddelvadi@hawk.iit.edu>
2017-04-29 15:09:02 -0500

    Combinewd first name and last name filed into one field and added Primary Key ID to keep track of each row and to make it easier to handle row data

--- scripts/Database/createdata.sql
+++ scripts/Database/createdata.sql
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS listride 
 (
-  FIRST_NAME varchar(100)    NOT NULL,
-  Last_NAME varchar(100)     NOT NULL,
+  ID MEDIUMINT  NOT NULL AUTO_INCREMENT,
+  NAME varchar(100)          NOT NULL,
   EMAIL varchar(100)         NOT NULL,
   ORIGIN varchar(200)        NOT NULL,
   DESTINATION varchar(200)   NOT NULL,
