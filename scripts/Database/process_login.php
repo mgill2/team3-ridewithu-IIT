89ad2db4f29cdf8aeeb0b71ca5f55d18baad106f
ddelvadi <ddelvadi@hawk.iit.edu>
2017-04-28 19:00:27 -0500

    Combined last name and first name

--- scripts/Database/process_login.php
+++ scripts/Database/process_login.php
@@ -12,8 +12,10 @@ if(isset($_POST) && !empty($_POST)) {
 
   if($count == 1) {
     $row = mysqli_fetch_assoc($result);
-    $_SESSION['first_name'] = $row["FIRST_NAME"];
-    $_SESSION['last_name'] = $row["LAST_NAME"];
+    //$_SESSION['first_name'] = $row["FIRST_NAME"];
+    //$_SESSION['last_name'] = $row["LAST_NAME"];
+    $name = $row["FIRST_NAME"] . ' ' . $row["LAST_NAME"]
+    $_SESSION['name'] = $name;
     $_SESSION['email'] = $row["EMAIL"];
     $_SESSION['username']=$username;
     header('Location: ../../src/welcome.php');
