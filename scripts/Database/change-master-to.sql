9840bcb6ecfbc2a80588b6ab23da1c41594b31ba
mgill2 <mgill5@hawk.iit.edu>
2017-05-01 19:43:41 -0500

    changed the master log file to 12

--- scripts/Database/change-master-to.sql
+++ scripts/Database/change-master-to.sql
@@ -2,7 +2,7 @@ CHANGE MASTER TO
 	master_host='192.168.1.220', 
 	master_user='replicate',
 	master_password='slaveReplicate',
-	master_log_file='mysql-bin.000001', 
+	master_log_file='mysql-bin.000012', 
   master_log_pos=107;
 
 use master;
