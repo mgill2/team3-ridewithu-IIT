4f1e47c8d18a30d1439372a73bbe9e8eacdc9ac1
ddelvadi <ddelvadi@hawk.iit.edu>
2017-05-01 20:47:06 -0500

    Provided correct IPs

--- scripts/Database/connectdatabase.php
+++ scripts/Database/connectdatabase.php
@@ -1,6 +1,6 @@
 <?php
 include_once 'config_database.php'; //Calling all the variables saved in this file
-$connection = mysqli_connect('192.168.1.221', 'team3rocks', 'jeremytheboss');
+$connection = mysqli_connect('192.168.1.220', 'team3rocks', 'jeremytheboss');
 if(!$connection) {
 	die("Database connection failed" . mysqli_error($connection));
 }
--- scripts/Database/createuser.sql
+++ scripts/Database/createuser.sql
@@ -1,5 +1,5 @@
-CREATE USER 'replicate'@'192.168.1.221' IDENTIFIED BY 'slaveReplicate';
-GRANT REPLICATION SLAVE ON *.* TO 'replicate'@'192.168.1.221';
-CREATE USER 'team3rocks'@'192.168.1.221' IDENTIFIED BY 'jeremytheboss';
-GRANT ALL ON *.* TO 'team3rocks'@'192.168.1.221';
+CREATE USER 'replicate'@'192.168.1.219' IDENTIFIED BY 'slaveReplicate';
+GRANT REPLICATION SLAVE ON *.* TO 'replicate'@'192.168.1.219';
+CREATE USER 'team3rocks'@'192.168.1.219' IDENTIFIED BY 'jeremytheboss';
+GRANT ALL ON *.* TO 'team3rocks'@'192.168.1.219';
 FLUSH PRIVILEGES;
