f595fe3e9bcf7288bc53491e43420411d5fe9434
Matt <matt@Yoda.attlocal.net>
2017-05-09 19:28:18 -0500

    added insert data for users

---
+++ scripts/Database/insert-data.sql
@@ -0,0 +1,5 @@
+use master;
+
+INSERT INTO users (ID, FIRST_NAME, LAST_NAME, EMAIL, USERNAME, PASSWORD) values 
+(0,"Joe", "Test", "jtest@example.com", "jtest", "Password"),
+(0,"Bob", "Test", "btest@example.com", "btest", "Password2");
